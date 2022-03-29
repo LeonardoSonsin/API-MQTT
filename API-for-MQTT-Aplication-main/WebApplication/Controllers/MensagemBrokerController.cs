a501d8b5af1f44f134d09d087ec82af73c187adb
LeonardoSonsin <70351101+LeonardoSonsin@users.noreply.github.com>
2022-03-29 19:47:02 -0300

    Add files via upload

---
+++ API-for-MQTT-Aplication-main/WebApplication/Controllers/MensagemBrokerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("Mensagem")]
+    public class MensagemBrokerController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+
+        public async Task<ActionResult<List<Msg>>> Get([FromServices] DataContext context)
+        {
+            var mensagens = await context.MensagemBrokers.ToListAsync();
+            return mensagens;
+        }
+
+        [HttpPost]
+        [Route("")]
+
+        public async Task<ActionResult<Msg>> Post ([FromServices] DataContext context, [FromBody] MensagemBroker model)
+        {
+            if(ModelState.IsValid)
+            {
+                context.MensagemBrokers.Add(model);
+                await context.SaveChangesAsync();
+                return model;
+                
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
---
+++ API-for-MQTT-Aplication-main/WebApplication/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
+
+namespace WebApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+        public DbSet<Msg> MensagemBrokers { get; set; }
+    }
+}
